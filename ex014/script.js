600fe4ecd235d22156a6fbb97d6a6e636d4d258e
Jonathan Sousa Pires <154604093+JhonSP13@users.noreply.github.com>
2025-04-02 15:21:49 -0300

    detalhes

--- ex014/script.js
+++ ex014/script.js
@@ -3,21 +3,22 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas!`
+    var minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minuto} !`
     if (hora >= 6 && hora < 12) {
-        msg.innerHTML = `Agora são ${hora} horas! Bom Dia.`
+        msg.innerHTML = `Agora são ${hora}:${minuto} ! Bom Dia.`
         img.src = 'imagens/manha.webp'
         document.body.style.background = '#FCC35D'
     } else if (hora >= 12 && hora < 18) {
-        msg.innerHTML = `Agora são ${hora} horas! Boa Tarde.`
+        msg.innerHTML = `Agora são ${hora}:${minuto} ! Boa Tarde.`
         img.src = 'imagens/tarde.jpeg'
         document.body.style.background = '#5F9B89'
     } else  if (hora >= 18 && hora < 23) {
-        msg.innerHTML = `Agora são ${hora} horas! Boa Noite.`
+        msg.innerHTML = `Agora são ${hora}:${minuto} ! Boa Noite.`
         img.src = 'imagens/crepusculo.jpg'
         document.body.style.background = '#063978'
     } else {
-        msg.innerHTML = `Agora são ${hora} horas! Boa Madrugada.`
+        msg.innerHTML = `Agora são ${hora}:${minuto} ! Boa Madrugada.`
         img.src = 'imagens/noite.jpeg'
         document.body.style.background = '#243231'
     }
