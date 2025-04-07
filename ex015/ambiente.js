c24ef06e8d75f2b9a61925ceab319b01e4bee658
Jonathan Sousa Pires <154604093+JhonSP13@users.noreply.github.com>
2025-04-07 09:39:35 -0300

    laço de depuraçao (for) JS

--- ex015/ambiente.js
+++ ex015/ambiente.js
@@ -1,10 +1,14 @@
-var c = 1
+//var c = 1
 /*while (c <= 3) {
     console.log(c)
     c++
 } */
 
-do {
+/* do {
     console.log(c)
     c++
-} while (c <= 10)
+} while (c <= 10) */
+
+for(var c = 1; c <= 20; c++) {
+    console.log(c)
+}
