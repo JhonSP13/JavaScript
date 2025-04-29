a6fa5e5f39d2c1654a6bf0457714e30dbc54d23b
Jonathan Sousa Pires <154604093+JhonSP13@users.noreply.github.com>
2025-04-29 08:50:54 -0300

    tentando solucionar problemas

--- ex018/script.js
+++ ex018/script.js
@@ -7,9 +7,16 @@ function add() {
     }   else if(valor < 0 || valor > 100) {
         window.alert('Digite um número entre 0 e 100!')
     }   else {
-        let n = document.createElement('option')
-        n.text = `Valor ${valor} adicionado`
-        n.value = `${valor}`
-        list.appendChild(n)
+        const vals = []
+        let i = 0
+        vals[i] = valor
+        if(vals.indexOf(vals[i]) == -1) {
+            window.alert('Esse número ja esta na lista.')
+        } else {
+            vals[i] = document.createElement('option')
+            vals[i].text = `Valor ${valor} adicionado`
+            vals[i].value = `${valor}`
+            list.appendChild(vals[i])
+        }
     }
 }
