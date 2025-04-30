73d394dc009fb26cc1a00bbeecd96243647e0c45
Jonathan Sousa Pires <154604093+JhonSP13@users.noreply.github.com>
2025-04-30 10:02:24 -0300

    corrigindo com professor p1

--- ex018/script.js
+++ ex018/script.js
@@ -1,22 +1,28 @@
-function add() {
-    var num = document.getElementById('num')
-    var valor = Number(num.value)
-    var list = document.getElementById('list')
-    if (num.value.length == 0) {
-        window.alert('Digite um valor válido entra 0 e 100!')
-    }   else if(valor < 0 || valor > 100) {
-        window.alert('Digite um número entre 0 e 100!')
-    }   else {
-        const vals = []
-        let i = 0
-        vals[i] = valor
-        if(vals.indexOf(vals[i]) == -1) {
-            window.alert('Esse número ja esta na lista.')
-        } else {
-            vals[i] = document.createElement('option')
-            vals[i].text = `Valor ${valor} adicionado`
-            vals[i].value = `${valor}`
-            list.appendChild(vals[i])
-        }
+let num = document.getElementById('fnum')
+let lista = document.getElementById('flista')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
 }
